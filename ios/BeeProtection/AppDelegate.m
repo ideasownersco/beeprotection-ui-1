1ece750a51f4d1d2d149d9dabe11b7adf83cf550
ZaL <z4ls@live.com>
2018-01-14 16:56:38 +0300

    fix project name

--- ios/BeeProtection/AppDelegate.m
+++ ios/BeeProtection/AppDelegate.m
@@ -31,7 +31,7 @@
   
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
-                                                      moduleName:@"beeprotection"
+                                                      moduleName:@"BeeProtection"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
