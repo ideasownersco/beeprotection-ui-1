0095f1f339844695ecaa4f96eee4d96ec70a122a
ZaL <z4ls@live.com>
2018-01-14 16:11:10 +0300

    link pushnotification

--- ios/BeeProtection/AppDelegate.m
+++ ios/BeeProtection/AppDelegate.m
@@ -8,7 +8,6 @@
  */
 
 #import "AppDelegate.h"
-#import <CodePush/CodePush.h>
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
@@ -21,11 +20,9 @@
 {
   NSURL *jsCodeLocation;
 
-    #ifdef DEBUG
-        jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
-    #else
-     jsCodeLocation = [CodePush bundleURL];
-    #endif
+
+  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"beeprotection"
