59112acbf73e7b34299dde3fce9c9608e0a3a959
ZaL <z4ls@live.com>
2018-10-11 17:53:43 +0400

    upgrading broken

--- android/app/src/main/java/com/beeprotection/MainApplication.java
+++ android/app/src/main/java/com/beeprotection/MainApplication.java
@@ -3,30 +3,17 @@ package com.beeprotection;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.learnium.RNDeviceInfo.RNDeviceInfo;
-import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
-import com.microsoft.codepush.react.CodePush;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+
 import java.util.Arrays;
 import java.util.List;
-import com.airbnb.android.react.maps.MapsPackage;
-import com.transistorsoft.rnbackgroundgeolocation.*;
-import com.transistorsoft.rnbackgroundfetch.RNBackgroundFetchPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-
-        @Override
-        protected String getJSBundleFile() {
-        return CodePush.getJSBundleFile();
-        }
-    
     @Override
     public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
@@ -35,15 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-          new RNBackgroundGeolocation(),
-          new RNBackgroundFetchPackage(),
-          new RNDeviceInfo(),
-          new RNI18nPackage(),
-          new CodePush(null, getApplicationContext(), BuildConfig.DEBUG),
-          new VectorIconsPackage(),
-          new ReactNativePushNotificationPackage(),
-          new MapsPackage()
+          new MainReactPackage()
       );
     }
 
