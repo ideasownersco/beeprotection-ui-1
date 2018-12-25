7bbc1430f80c7c543b8ec6e9677505c0925b6c10
ZaL <z4ls@live.com>
2018-12-25 12:52:00 +0300

    driver track

--- src/company/drivers/TrackScene.js
+++ src/company/drivers/TrackScene.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {ACTIONS as DRIVER_ACTIONS} from 'company/common/actions';
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
-import {Image, Dimensions, View, Text, WebView} from 'react-native';
+import {Image, Dimensions, View, Text, AppState} from 'react-native';
 import images from 'assets/theme/images';
 import {SELECTORS as COMPANY__DRIVER_SELECTORS} from 'company/selectors/drivers';
 import Button from 'components/Button';
@@ -12,9 +12,6 @@ const ASPECT_RATIO = width / height;
 const LATITUDE_DELTA = 0.3;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 import I18n from 'utils/locale';
-import Platform from 'react-native';
-
-const DEFAULT_PADDING = {top: 100, right: 100, bottom: 100, left: 100};
 
 class TrackScene extends PureComponent {
   static propTypes = {
@@ -27,10 +24,12 @@ class TrackScene extends PureComponent {
 
   state = {
     pauseTrackingUpdate: false,
-    initialized:false
+    initialized:false,
+    appState: AppState.currentState
   };
 
   componentDidMount() {
+    AppState.addEventListener('change', this._handleAppStateChange);
     this.props.dispatch(DRIVER_ACTIONS.fetchDrivers());
     this.props.dispatch(DRIVER_ACTIONS.subscribeToDriverTrackings());
 
@@ -41,27 +40,20 @@ class TrackScene extends PureComponent {
     },1000)
   }
 
-  onDriversListItemPress = (driver: object) => {
-    this.props.navigation.navigate('DriverDetail', {
-      driverID: driver.id,
-    });
+  componentWillUnmount() {
+    AppState.removeEventListener('change', this._handleAppStateChange);
+  }
+
+  _handleAppStateChange = (nextAppState) => {
+    if (this.state.appState.match(/inactive|background/) && nextAppState === 'active') {
+      this.props.dispatch(DRIVER_ACTIONS.subscribeToDriverTrackings());
+    }
+    this.setState({appState: nextAppState});
   };
 
   onMapLayout = () => {
     this.map.fitToElements(true);
-    // this.map.fitToCoordinates(this.props.drivers, {
-    //   edgePadding: DEFAULT_PADDING,
-    //   animated: true,
-    // });
   };
-
-  componentDidUpdate(nextProps) {
-    // if (this.props.drivers !== nextProps.drivers) {
-    //   if (!this.state.pauseTrackingUpdate) {
-    //     this.onMapLayout();
-    //   }
-    // }
-  }
 
   resumeTrackingUpdate = () => {
     this.setState({
@@ -80,65 +72,17 @@ class TrackScene extends PureComponent {
   render() {
     let {drivers} = this.props;
 
-    // drivers.map(driver => {
-    //   console.log('driver.latitude',driver.latitude);
-    // });
-
     let origin = {
-      // latitude: 37.48522,
-      // longitude: -122.23635,
       latitude: 29.3772392006689,
       longitude: 47.98511826155676,
       heading: 0,
       latitudeDelta: LATITUDE_DELTA,
       longitudeDelta: LONGITUDE_DELTA,
     };
-    //
-    // let drivers = [
-    //   {
-    //     driver_id: 1,
-    //     heading: 305.16,
-    //     job_id: '1',
-    //     latitude: 37.49844611,
-    //     longitude: -122.31576031,
-    //   },
-    //   {
-    //     driver_id: 2,
-    //     heading: 305.16,
-    //     job_id: '2',
-    //     latitude: 37.33182,
-    //     longitude: -122.03118,
-    //   },
-    // ];
-
-    // let origin = {
-    //   latitude: 29.378586,
-    //   longitude: 47.990341,
-    //   latitudeDelta: 1,
-    //   longitudeDelta: 1,
-    // };
-
-    // let drivers = [
-    //   {
-    //     driver_id: 1,
-    //     heading: 305.16,
-    //     job_id: '1',
-    //     latitude: 29.33285,
-    //     longitude: 48.05415,
-    //   },
-    //   {
-    //     driver_id: 2,
-    //     heading: 305.16,
-    //     job_id: '1',
-    //     latitude: 29.3195616,
-    //     longitude: 47.991724
-    //   },
-    // ];
 
     return (
       <View style={{flex: 1}}>
         <MapView
-          // provider={PROVIDER_GOOGLE}
           ref={ref => {
             this.map = ref;
           }}
@@ -152,53 +96,9 @@ class TrackScene extends PureComponent {
           onPress={this.pauseTrackingUpdate}
         >
           {drivers.map((driver, index) => {
-            // const {heading} = driver;
-            // const rotate =
-            //   typeof heading === 'number' && heading >= 0
-            //     ? `${heading}deg`
-            //     : undefined;
-            // return (
-            //   <MapView.Marker
-            //     key={`${index}`}
-            //     anchor={{x: 0.5, y: 0.5, position: 'relative'}}
-            //     coordinate={{...driver}}
-            //     identifier="MarkerOrigin"
-            //     mapPadding={5}
-            //   >
-            //     <View>
-            //       <MapView.Callout tooltip style={{minWidth: 100, maxWidth: 300}}>
-            //           <Text style={{fontSize:19,fontWeight:'500',color:'red'}}>Driver</Text>
-            //       </MapView.Callout>
-            //
-            //       <Image
-            //         source={images.car}
-            //         style={[
-            //           {
-            //             width: 20,
-            //             height: 40,
-            //           },
-            //           // rotate && {transform: [{rotate}]},
-            //         ]}
-            //       />
-            //     </View>
-            //   </MapView.Marker>
-            // );
-
-            console.log('drivers',drivers);
-
-
             if(this.state.initialized) {
               return (
                 <MapView.Marker
-                  // provider={PROVIDER_GOOGLE}
-                  // ref={'ref' + driver._id}
-                  // key={'key' + driver._id}
-                  // // anchor={{x: 0.5, y: 0.5, position: 'relative'}}
-                  // coordinate={{...driver}}
-                  // tracksViewChanges={true}
-                  // identifier="MarkerOrigin"
-                  // mapPadding={5}
-
                   key={`${index}`}
                   anchor={{x: 0.5, y: 0.5, position: 'relative'}}
                   coordinate={{...driver}}
@@ -206,49 +106,23 @@ class TrackScene extends PureComponent {
                   mapPadding={5}
                 >
                   <Image
-                    // source={{uri: property.images[0]}}
-                    // style={[styles.image]}
                     source={images.car}
                     style={[
                       {
                         width: 20,
                         height: 40,
                       },
-                      // rotate && {transform: [{rotate}]},
                     ]}
                     resizeMode="cover"
                   />
                   <MapView.Callout onPress={() => {}} tracksViewChanges={true}>
                     <View style={{flex:1,width:100,height:30,justifyContent:'center'}}>
-                      <Text style={{fontSize:19}}>{driver.user ? driver.user.name : 'Driver'}</Text>
+                      <Text style={{fontSize:19}}>{driver.user ? driver.user.name : `Driver ${driver.id}`}</Text>
                     </View>
                   </MapView.Callout>
                 </MapView.Marker>
               );
-
             }
-
-
-            // return (
-            //   <MapView.Marker
-            //     key={`${index}`}
-            //     anchor={{x: 0.5, y: 0.5, position: 'relative'}}
-            //     coordinate={{...driver}}
-            //     identifier="MarkerOrigin"
-            //     mapPadding={5}
-            //   >
-            //     <Image
-            //       source={images.car}
-            //       style={[
-            //         {
-            //           width: 20,
-            //           height: 40,
-            //         },
-            //         // rotate && {transform: [{rotate}]},
-            //       ]}
-            //     />
-            //   </MapView.Marker>
-            // );
           })}
         </MapView>
 
