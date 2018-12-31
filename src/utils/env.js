c3798238ab34085638a609c53f2273614850ea4c
ZaL <z4ls@live.com>
2018-12-31 21:34:15 +0530

    fix

--- src/utils/env.js
+++ src/utils/env.js
@@ -3,19 +3,18 @@ const defaults = {};
 if (__DEV__) {
   module.exports = {
     ...defaults,
+    // NETWORK_PROTOCOL: 'http://',
+    // API_URL: 'beeprotection.test/api',
+    // WEB_URL: 'http://beeprotection.test',
+    // // API_URL: '192.168.1.102:8081/api',
+    // SOCKET_SERVER: 'http://beeprotection.test:3000',
+    // PAYMENT_ENDPOINT: 'http://beeprotection.test/payment/knet',
 
-    NETWORK_PROTOCOL: 'http://',
-    API_URL: 'beeprotection.test/api',
-    WEB_URL: 'http://beeprotection.test',
-    // API_URL: '192.168.1.102:8081/api',
-    SOCKET_SERVER: 'http://beeprotection.test:3000',
-    PAYMENT_ENDPOINT: 'http://beeprotection.test/payment/knet',
-
-    // NETWORK_PROTOCOL: 'https://',
-    // API_URL: 'beeprotection.net/api',
-    // WEB_URL: 'https://beeprotection.net',
-    // SOCKET_SERVER: 'http://beeprotection.net:3000',
-    // PAYMENT_ENDPOINT: 'https://beeprotection.net/payment/knet',
+    NETWORK_PROTOCOL: 'https://',
+    API_URL: 'beeprotection.net/api',
+    WEB_URL: 'https://beeprotection.net',
+    SOCKET_SERVER: 'http://beeprotection.net:3000',
+    PAYMENT_ENDPOINT: 'https://beeprotection.net/payment/knet',
 
     GOOGLE_MAPS_KEY: 'AIzaSyCpQX4H0QPxVgKuNMZ0ELG_ymgT8RHcKh4',
     GOOGLE_MAPS_IOS_KEY: 'AIzaSyDPCgdWqrkBe4v3uSuU-MZGJIZ0AQxfbCo',
